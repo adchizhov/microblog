e2bc8de321f2d7df615eaa5dfa1e71db245c77d1
adchizhov <adchizhov@mail.ru>
2016-11-01 14:11:56 +0300

    added so much things

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 __author__ = 'adchizhov'
 
 from app import db
+from hashlib import md5
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -10,7 +11,52 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index =True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    role = db. Column(db.SmallInteger, default=ROLE_USER)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname = nickname).first() == None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname = new_nickname).first() == None:
+                break
+            version += 1
+        return new_nickname
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id) # python2!
+        except:
+            return str(self.id) # python3!
 
     def __repr__(self):
         return '<User {}>'.format(self.nickname)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/{}?d=mm&s={}'.format(md5(self.email).hexdigest(), str(size))
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(150))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
---
+++ runp.py
@@ -0,0 +1,6 @@
+# -*- coding:UTF-8 -*-
+__author__ = 'adchizhov'
+
+from app import app
+
+app.run(debug=False)
